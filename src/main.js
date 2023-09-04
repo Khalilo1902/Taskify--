440c2a15977df3a1d1c88abb110baded9524395d
Khalil <Khalil.haouas@gmail.com>
2023-09-04 13:41:01 +0200

    Button Go korrigiert

--- src/main.js
+++ src/main.js
@@ -1,85 +1,48 @@
 import "../src/style.css";
 
 document.querySelector("#app").innerHTML = /*html */ `
-  <div>
-<h1 class =" text-sky-900 text-[5rem] text-center">Taskify</h1>
+<div>
+          
+  <h1 class =" text-sky-900 text-[5rem] text-center">Taskify</h1>
   
-  <div class = "flex items-center justify-center mt-[20rem]">
-  <div class =" border border-slate-700 w-fit rounded-lg">
-    <input type="text" class =" border border-none outline-none">
-    <button id="btnOne" class=" bg-slate-300 w-10 px-3 py-2 rounded-r-lg" >Go!</button>
+  <div  class = "flex items-center justify-center mt-[20rem]">
+     <div class =" border border-slate-700 w-fit rounded-lg">
+      <input type="text" id="taskInput" class =" border border-none outline-none">
+      <button id="btnGo" class=" bg-slate-300 w-10 px-3 py-2 rounded-r-lg" >Go!</button>
+     </div>
   </div>
   
 </div>
-<div>
+  
 <ul class = "flex items-center justify-center flex-col mt-10  gap-6">
-    <li ></li>
    
-  </ul>
-</div>
+</ul>
+
 
 </div>
 `;
-const input = document.querySelector("input");
+const input = document.querySelector("#taskInput");
+const ul = document.querySelector("ul");
 
-const listElement = document.querySelector("li");
-
-const button = document.querySelector("#btnOne");
-button.addEventListener("click", () => {
-
+document.querySelector("#btnGo").addEventListener("click", () => {
   const inputValue = input.value;
-  listElement.innerHTML += /*html */ `
-  <li class = " bg-slate-500 w-fit p-2 rounded-lg text-center border border-b-4 ">
-  ${inputValue} <span class="trash"> <i class =" fa-solid fa-trash ml-[3rem]" style="color: #e600b4;">
+  if (inputValue.trim() !== "") {
+    const li = document.createElement("li");
+    ul.appendChild(li);
+    li.className = "bg-slate-500 p-2 rounded-lg text-center border border-b-4";
+    li.innerHTML += inputValue;
+    const icon1Elem = document.createElement("i");
+    icon1Elem.className = "icon-trash fa-solid fa-trash ml-3 text-red-500";
+    li.appendChild(icon1Elem);
+    const icon2Elem = document.createElement("i");
+    icon2Elem.className = "fa-solid fa-pencil ml-4 text-red-500";
+    li.appendChild(icon2Elem);
 
-  </i>
-</span>
-<span id="pencil">
-<i class=" ml-4 fa-solid fa-pencil" style="color: #e600b4;"></i>
-</span>
-  </li>
-  `;
-  input.value = "";
+    
+    
 
-  const spanElement = document.querySelector(".trash");
-
-
-
-spanElement.addEventListener("click", () => {
-  
-  spanElement.parentNode.remove()
-  
+    input.value = "";
+  }
 });
 
-const pencilSpan = document.querySelector("#pencil")
-
-
-pencilSpan.addEventListener("click",()=>{
-  
-  const createInput = document.createElement("input")
-  const listElementPencil = document.createElement("li")
-  console.log(listElementPencil);
-  listElementPencil.appendChild(createInput)
-  
-  console.log(listElementPencil);
-  
-  listElementPencil.innerHTML += /*html */`
-  <li class =" bg-slate-500 w-fit p-2 rounded-lg text-center border border-b-4 ">${createInput.outerHTML} <button id="okButton">ok</button> </li>
-  
-  `
-  listElement.replaceWith(listElementPencil)
-})
-const okButton = document.querySelector("#okButton")
-
-okButton.addEventListener("click",(e)=>{
-  e.target.className = 'okButton'
-console.log("khalil")
-  inputValue.replaceWith(createInput.value)
-
-
-  
-
-})
-
-});
-
+const iconTrash = document.querySelector(".icon-trash");
